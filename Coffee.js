acc69c927b0defd3d35940114bc2c1b385b0fa27
lucasfcosta <fernandesdacostalucas@gmail.com>
2016-05-20 16:47:52 -0300

    Coffee prototype for lucas_costa

--- Coffee.js
+++ Coffee.js
@@ -1,4 +1,25 @@
 function Coffee() {
+    this.empty = true;
 }
+
+Coffee.prototype.refill = function() {
+    if (!this.empty) {
+        throw new Error('Coffee cup is already full');
+    }
+
+    this.empty = false;
+};
+
+Coffee.prototype.drink = function() {
+    if (this.empty) {
+        throw new Error('Coffee cup is empty');
+    }
+
+    this.empty = true;
+};
+
+Coffee.prototype.isEmpty = function() {
+    return this.empty;
+};
 
 module.exports = Coffee;
