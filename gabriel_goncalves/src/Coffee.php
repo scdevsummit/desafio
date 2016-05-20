5eb2a96e774020be8f99aa6ea536e55eb10db868
gabriel.goncalves <gabriel.goncalves@fidelize.com.br>
2016-05-20 16:56:44 -0300

    * Implementando classe e fazendo o teste passar -  GREEN

--- gabriel_goncalves/src/Coffee.php
+++ gabriel_goncalves/src/Coffee.php
@@ -9,13 +9,23 @@ class Coffee
 
     public function refill()
     {
+        return $this->isEmpty = false ;
     }
 
     public function drink()
     {
+        return $this->isEmpty = true;
     }
 
     public function isEmpty()
     {
+        if ($this->isEmpty) {
+            $this->refill();
+        } else {
+            $this->drink();
+        }
+
+        return $this->isEmpty;
+
     }
 }
