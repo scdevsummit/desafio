63181d816395575b099173b3470f3880c8ea1d2a
Fernando Daciuk <f.daciuk@gmail.com>
2016-05-21 16:28:42 -0300

    Check if isEmpty method exists

--- fernando-daciuk/test.js
+++ fernando-daciuk/test.js
@@ -7,3 +7,8 @@ const Coffee = require('./coffee')
 test('Should there is the Coffee class', () => {
   expect(Coffee).to.be.a('function')
 })
+
+test('Should Coffee has a method isEmpty', () => {
+  const coffee = new Coffee()
+  expect(coffee.isEmpty).to.be.a('function')
+})
