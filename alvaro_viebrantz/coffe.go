f4c95ac041521c3f6674b18c6335e64dd5af86e0
Alvaro Viebrantz <alvarowolfx@gmail.com>
2016-05-20 21:01:45 -0400

    [feat] Add coffee struct with a coffee creator and check if empty method

---
+++ alvaro_viebrantz/coffe.go
@@ -0,0 +1,16 @@
+package main
+
+// Coffee a simply coffee struct
+type Coffee struct {
+	empty bool
+}
+
+// NewCoffee Creates a new coffee
+func NewCoffee() *Coffee {
+	return &Coffee{true}
+}
+
+// IsEmpty check if the coffee is empty
+func (c Coffee) IsEmpty() bool {
+	return c.empty
+}
