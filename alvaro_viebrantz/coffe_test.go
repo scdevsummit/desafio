1c207225ecd46e0665220ecc9aedcfdf362217cd
Alvaro Viebrantz <alvarowolfx@gmail.com>
2016-05-20 21:10:13 -0400

    [test] Coffe should have a drink method

--- alvaro_viebrantz/coffe_test.go
+++ alvaro_viebrantz/coffe_test.go
@@ -2,20 +2,31 @@ package main
 
 import "testing"
 
-func TestIfCoffeIsEmpty(t *testing.T) {
+func TestIfCoffeeIsEmpty(t *testing.T) {
 	coffee := NewCoffee()
 
 	if !coffee.IsEmpty() {
-		t.Error("Coffe should start empty")
+		t.Error("Coffee should start empty")
 	}
 }
 
-func TestIfCanRefillCoffe(t *testing.T) {
+func TestIfCanRefillCoffee(t *testing.T) {
 	coffee := NewCoffee()
 
 	coffee.Refill()
 
 	if coffee.IsEmpty() {
 		t.Error("Coffee should be full after refill")
+	}
+}
+
+func TestIfCanDrinkCoffee(t *testing.T) {
+	coffee := NewCoffee()
+
+	coffee.Refill()
+	coffee.Drink()
+
+	if !coffee.IsEmpty() {
+		t.Error("Coffee should be empty after drinking it")
 	}
 }
