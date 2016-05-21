75452dc2b7e3eb6c85f5af7ea385c96b370e505f
Lucas dos Santos Abreu <lucas.s.abreu@gmail.com>
2016-05-21 13:19:06 -0300

    Etapa 2

--- lucas_santos_abreu/tests/LucasSantosAbreu/CoffeTest/CoffeTest.php
+++ lucas_santos_abreu/tests/LucasSantosAbreu/CoffeTest/CoffeTest.php
@@ -8,10 +8,12 @@ class CoffeTest extends \PHPUnit_Framework_TestCase {
 
 	/**
 	 * @convers \LucasSantosAbreu\Coffe\Coffe::__construct
-	 * 
+	 * @convers \LucasSantosAbreu\Coffe\Coffe::isEmpty
 	 */
-	public function testCanCreateACoffeMug () {
+	public function testIsCoffeMugEmptyWhenCreated() {
 		$mug = new Coffe();
+		$isEmpty = $mug->isEmpty();
+		$this->assertEquals($isEmpty, true); 
 	}
 
 }
